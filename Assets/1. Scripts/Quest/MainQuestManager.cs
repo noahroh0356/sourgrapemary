3de6c397dce6e2c9abf4bccb44492a19ff19d3f5
noahroh0356 <slop0356@naver.com>
2025-03-13 21:04:52 +0900

    mainquest작업

--- Assets/1. Scripts/Quest/MainQuestManager.cs
+++ Assets/1. Scripts/Quest/MainQuestManager.cs
@@ -9,6 +9,9 @@ public class MainQuestManager : MonoBehaviour
     public UserMainQuest userMainQuest;
     public int curQuestIndex = 0; // 퀘스트가 차례대로 나타나도록 설정=진행 중인 퀘스트
 
+    public string[] purchaseFurnitureQuestKeys;
+    public string[] purchaseKitchenQuestKeys;
+
     private void Awake()
     {
         Instance = this;
@@ -16,7 +19,23 @@ public class MainQuestManager : MonoBehaviour
 
     private void Start()
     {
-        StartQuest();
+        userMainQuest = SaveMgr.LoadData<UserMainQuest>("UserMainQuest");
+        if (userMainQuest == null)
+        {
+            userMainQuest = new UserMainQuest();
+        }
+
+        if (userMainQuest.processing == false)
+        {
+
+            StartQuest();
+        }
+
+        else
+        {
+            MainQuestData data = GetMainQuestData(userMainQuest.mainQuestType);
+            FindObjectOfType<MainQuestPanel>().StartQuest(data);
+        }
     }
 
 
@@ -26,7 +45,18 @@ public class MainQuestManager : MonoBehaviour
         MainQuestData data = mainQuestDatas[curQuestIndex];
         //MainQuestData data = mainQuestDatas[Random.Range(0, mainQuestDatas.Length)];
 
-        userMainQuest = new UserMainQuest();
+        //userMainQuest = new UserMainQuest();
+
+        if (data.mainQuestType == MainQuestType.PurchaseFurniture)
+        {
+          string nextGoal = data.GetGoal();
+            if (nextGoal == null)
+            {
+                curQuestIndex++;
+                StartQuest();
+                return;
+            }
+                }
         userMainQuest.mainQuestType = data.mainQuestType;
         FindObjectOfType<MainQuestPanel>().StartQuest(data);
     }
@@ -46,13 +76,30 @@ public void DoQuest(MainQuestType type)
     {
         MainQuestData curQuestData = GetMainQuestData(userMainQuest.mainQuestType);
 
-        if (curQuestData != null && userMainQuest.process >= curQuestData.goal)
+        if (userMainQuest.mainQuestType == MainQuestType.PurchaseFurniture)
         {
-            Debug.Log("퀘스트 완료!");
-            curQuestIndex++;  // 다음 퀘스트로 인덱스 증가
-            User.Instance.AddExp(1);
-            StartQuest();         // 다음 퀘스트 시작
-            return true;
+           string furnitureKey = curQuestData.GetGoal();
+           UserFurniture userFurniture = User.Instance.GetUserFurniture(furnitureKey);
+            return userFurniture.purchased;
+        }
+        else if (userMainQuest.mainQuestType == MainQuestType.PurchaseKitchen)
+        {
+            string KitchenKey = curQuestData.GetGoal();
+            UserKitchen userKitchenBar = User.Instance.GetUserKitchen(KitchenKey);
+            return userKitchenBar.purchased;
+
+        }
+
+        else
+        { 
+            if (curQuestData != null && userMainQuest.process >= curQuestData.goal)
+            {
+                Debug.Log("퀘스트 완료!");
+                curQuestIndex++;  // 다음 퀘스트로 인덱스 증가
+                User.Instance.AddExp(1);
+                StartQuest();         // 다음 퀘스트 시작
+                return true;
+            }
         }
 
         return false;
@@ -97,10 +144,14 @@ public MainQuestData GetMainQuestData(MainQuestType type)
 }
 
 [System.Serializable]
-public class UserMainQuest
+public class UserMainQuest // 현재 유저가 진행 중인 퀘스트에 대한 정보
 {
     public MainQuestType mainQuestType;
     public int process;
+
+    public bool processing;
+    public int clearPurchaseFurnitureCount;
+    public int clearPurchaseKitchenCount;
 }
 
 
@@ -111,15 +162,39 @@ public class MainQuestData
     public int goal;
     public string title;
     //public int exp;
+    public string GetGoal()
+    {
+        if (mainQuestType == MainQuestType.PurchaseFurniture)
+        {
+            int idx = MainQuestManager.Instance.userMainQuest.clearPurchaseFurnitureCount;
+
+            if (idx >= MainQuestManager.Instance.purchaseFurnitureQuestKeys.Length)
+                return null;
+
+            return MainQuestManager.Instance.purchaseFurnitureQuestKeys[idx];
+        }
+
+        else if (mainQuestType == MainQuestType.PurchaseKitchen)
+        {
+            int idx = MainQuestManager.Instance.userMainQuest.clearPurchaseKitchenCount;
+
+            if (idx >= MainQuestManager.Instance.purchaseKitchenQuestKeys.Length)
+                return null;
+
+            return MainQuestManager.Instance.purchaseKitchenQuestKeys[idx];
+        }
+
+        return null;
+    }
 
 }
 
 public enum MainQuestType
 {
 CallCustomer,
-TakeOrder,
-PickUpAcon,
+TakeOrder, // 설정
+PickUpAcon, //설정 
 PurchaseFurniture,
 PurchaseKitchen,
-PlayGatcha,
+PlayGatcha, // 설정
 }
